6fe0356556900dd5886a020eb641b470d5621347
mario carvalho <mario.carvalho.devpython@gmail.com>
2024-06-26 14:49:28 -0300

    experience

--- portifolio_ms/src/components/Experience/Experience.jsx
+++ portifolio_ms/src/components/Experience/Experience.jsx
@@ -1,16 +1,30 @@
 import "./Experience.css"
 
 function Experience() {
-  return (
-    <div className="experience">
-        <div className="archievement">
-        <div className="circle">8+</div>
+    return (
+        <div className="experience">
+            <div className="archievement">
+                <div className="circle">8+</div>
+                <span>anos </span>
+                <span>Experiencia</span>
+
+            </div>
+            <div className="archievement">
+                <div className="circle">20+</div>
+                <span>anos </span>
+                <span>Experiencia</span>
+
+            </div>
+            <div className="archievement">
+                <div className="circle">5+</div>
+                <span>anos </span>
+                <span>Experiencia</span>
+
+            </div>
+
 
         </div>
-
-
-    </div>
-  )
+    )
 }
 
 export default Experience
